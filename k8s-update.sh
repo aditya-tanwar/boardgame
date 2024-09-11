e14c646fce3f3b2cbd669d563d53646cf99c8533
Aditya Tanwar <aaditanwar0@gmail.com>
2024-09-11 14:30:40 +0530

    updating the manifests file

--- k8s-update.sh
+++ k8s-update.sh
@@ -6,7 +6,7 @@
 appversion=1
 
 
-sed -i "s|image: .*|image: adityatanwar03/boardgame:v$appversion|g" deployment-service.yaml
+sed -i "s|image: .*|image: adityatanwar03/boardgame:v$appversion|g" k8s/deployment-service.yaml
 
 
 exit 0

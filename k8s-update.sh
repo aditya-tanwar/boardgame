ab7ad375e4802d976e47d031b78ad5315c5bccad
Aditya Tanwar <86700210+aditya-tanwar@users.noreply.github.com>
2024-09-11 14:02:09 +0530

    Update k8s-update.sh

--- k8s-update.sh
+++ k8s-update.sh
@@ -6,7 +6,7 @@
 appversion=1
 
 
-sed -i "s|image: .*|image: adityatanwar03/boardgame:v$appversion|g" deployment-servicec.yaml
+sed -i "s|image: .*|image: adityatanwar03/boardgame:v$appversion|g" deployment-service.yaml
 
 
 exit 0
